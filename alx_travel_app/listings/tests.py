6ae04198cd0b18edca7f87aafa7dad9d9b7f58c5
Yohannes <Yohannesabesheger@users.noreply.github.com>
2025-07-14 16:14:04 +0300

    03working

--- alx_travel_app/listings/tests.py
+++ alx_travel_app/listings/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from celery import shared_task
+from django.core.mail import send_mail
 
-# Create your tests here.
+@shared_task
+def send_booking_confirmation_email(email, name, booking_id):
+    subject = 'Booking Confirmation'
+    message = f"Hello {name},\n\nYour booking with ID {booking_id} has been confirmed. Thank you!"
+    from_email = None  # use DEFAULT_FROM_EMAIL
+    recipient_list = [email]
+
+    send_mail(subject, message, from_email, recipient_list)
